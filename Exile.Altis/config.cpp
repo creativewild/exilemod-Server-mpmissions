df7fbbfad1ed08f10dd5cf03155f715f518d1f3d
SamuelFaith <azryal@live.com.au>
2015-08-24 16:37:34 +1000

    Update config.cpp
    One heli test

--- Exile.Altis/config.cpp
+++ Exile.Altis/config.cpp
@@ -1331,7 +1331,12 @@ class CfgExileArsenal
 	class Exile_Chopper_Hummingbird_Civillian_Vrana				{ quality = 1; price = 5000; };
 	class Exile_Chopper_Hummingbird_Civillian_Wasp				{ quality = 1; price = 5000; };
 	class Exile_Chopper_Hummingbird_Civillian_Wave				{ quality = 1; price = 5000; };
-
+	
+	///////////////////////////////////////////////////////////////////////////////
+        // Hellcat
+	///////////////////////////////////////////////////////////////////////////////
+	class I_Heli_light_03_F 						{ quality = 1; price = 15000; };
+	
 	///////////////////////////////////////////////////////////////////////////////
 	// Huron
 	///////////////////////////////////////////////////////////////////////////////
@@ -2390,6 +2395,7 @@ class CfgTraderCategories
 			"Exile_Chopper_Mohawk_FIA",
 			"Exile_Chopper_Huron_Black",
 			"Exile_Chopper_Hellcat_Green"
+			"class I_Heli_light_03_F"
 		};
 	};
 
